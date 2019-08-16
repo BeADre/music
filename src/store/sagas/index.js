8e4873faa5af86ee06674a9754bf3965aaea2266
BeADre <beadre_wang@163.com>
2019-08-16 18:12:48 +0800

    修改saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,6 +2,23 @@
 import {sagaAction} from "../actions"
 import {call, put,takeEvery} from "redux-saga/effects"
 
+
+// const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+//   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+//   const value = modulesFiles(modulePath)
+//   modules[moduleName] = value.default
+//   return modules
+// }, {})
+
+// var imagesContext = require.context('./store/actions/', false, /\.js$/);
+// console.log(imagesContext(imagesContext.keys()[0]))
+
+const modulesFiles = require.context('../actions/', false, /\.js$/);
+const sagaModule = modulesFiles.keys().reduce((accumulator,filename) => {
+  const funcName = filename.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const sagaAction = modulesFiles(filename)
+},{})
+
 export function* testSaga() {
   yield takeEvery(sagaAction.FIRST_ACTION_SAGA, function* () {
     try {
