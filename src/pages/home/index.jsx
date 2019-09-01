a5d6fb0972f0e60691a54bf63dc928cd0fabf709
BeADre <beadre_wang@163.com>
2019-09-02 01:21:22 +0800

    完成new song

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,16 @@
-import React, {useState, useEffect, useRef} from "react"
+import React, {useState, useEffect, useRef, Fragment} from "react"
 import {Carousel} from "antd"
 import usePlaylist from "../../customHook/usePlaylist"
 import {connect} from "react-redux"
 import {newSongTab} from "../../staticData/home"
 import "./index.scss"
 
-const Home = ({dispatch, tabState, playlistState}) => {
+const Home = ({dispatch, tabState, playlistState,newSongState}) => {
   const {playTab = []} = tabState;
   const {playlist = []} = playlistState;
-  const chooseIndex = useRef(null);
+  const {newSong = []} = newSongState;
+  const choosePlaylist = useRef(null);
+  const chooseNewSong = useRef(null);
   const [tabColorIndexPlaylist, setTabColorIndexPlaylist] = useState(0);
   const [playlistCat, setPlaylistCat] = useState("");
   const [tabColorIndexNewSong, setTabColorIndexNewSong] = useState(0);
@@ -27,8 +29,75 @@ const Home = ({dispatch, tabState, playlistState}) => {
     setCatName(name)
   }
 
+
+  // 第一部分遍历的歌单推荐内容
+  const playTabCarousel = (start,end)=>{
+    return (
+      <div className="slide">
+        {playlist.slice(start, end).map(value => (
+          <div key={value.id} className="slideContent">
+            <div className="slideContent-top">
+              <div className="slide-keep">
+                <span className="iconfont icon-ziyuan"> </span>
+              </div>
+              <img src={value.coverImgUrl} alt=""/>
+            </div>
+            <div className="slideContent-bot">
+              <a href="">{value.name}</a>
+              <p>播放量：{(value.playCount / 10000).toFixed(1)}万</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  // 第二部分遍历的新歌内容
+  const newSongCarousel = (start,end)=>{
+    return (
+      <div className="slide">
+        {newSong.slice(start,end).map((value) =>
+          <div  className="slideContent2" key={value.id}>
+            <div className="slideContent-left">
+              <div className="slide-keep">
+                <span className="iconfont icon-ziyuan"> </span>
+              </div>
+              <img src={value.album.picUrl} alt=""/>
+            </div>
+            <div className="slideContent-right">
+              <div className="right-detail">
+                <p>
+                  <a href="#">{value.name}</a>
+                </p>
+                <p>
+                  {value.artists.map((artist,index)=>
+                    <Fragment key={artist.id}>
+                      <a href="">{artist.name}</a>
+                      {index === value.artists.length - 1 ? "": <span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>}
+                    </Fragment>
+                  )}
+                </p>
+              </div>
+              <div className="song-time">{unitTime(value.duration)}</div>
+            </div>
+          </div>
+        )}
+
+      </div>
+    )
+  }
+
+
+  const unitTime = time => {
+    let minute = new Date(time).getMinutes();
+    let second = new Date(time).getSeconds();
+    minute = minute < 10 ? `0${minute}`: `${minute}` ;
+    second = second < 10 ? `0${second}`: `${second}` ;
+    return `${minute}:${second}`
+  }
+
   return (
-    <div className="homeContainer">
+    <div className="home-container">
       <div className="recommend-playlist">
         <h1>歌单推荐</h1>
         <div className="tab">
@@ -44,60 +113,45 @@ const Home = ({dispatch, tabState, playlistState}) => {
         </div>
         <div className="slide-container" style={{height: "410px"}}>
           <div className="prev-button">
-            <span className="iconfont icon-shangyiye" onClick={() => chooseIndex.current.prev()}/>
+            <span className="iconfont icon-shangyiye" onClick={() => choosePlaylist.current.prev()}/>
           </div>
           <div className="next-button">
-            <span className="iconfont icon-xiayiye" onClick={() => chooseIndex.current.next()}/>
+            <span className="iconfont icon-xiayiye" onClick={() => choosePlaylist.current.next()}/>
           </div>
-
-          <Carousel ref={chooseIndex}>
-            <div className="slide">
-              {playlist.slice(0, 5).map(value => (
-                <div key={value.id} className="slideContent">
-                  <div className="slideContent-top">
-                    <div className="slide-keep">
-                      <span className="iconfont icon-ziyuan"> </span>
-                    </div>
-                    <img src={value.coverImgUrl} alt=""/>
-                  </div>
-                  <div className="slideContent-bot">
-                    <a href="">{value.name}</a>
-                    <p>播放量：{(value.playCount / 10000).toFixed(1)}万</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="slide">
-              {playlist.slice(5, 10).map(value => (
-                <div key={value.id} className="slideContent">
-                  <div className="slideContent-top">
-                    <div className="slide-keep">
-                      <span className="iconfont icon-ziyuan"> </span>
-                    </div>
-                    <img src={value.coverImgUrl} alt=""/>
-                  </div>
-                  <div className="slideContent-bot">
-                    <a href="">{value.name}</a>
-                    <p>播放量：{(value.playCount / 10000).toFixed(1)}万</p>
-                  </div>
-                </div>
-              ))}
-            </div>
+          <Carousel ref={choosePlaylist}>
+            {playTabCarousel(0,5)}
+            {playTabCarousel(5,10)}
           </Carousel>
         </div>
       </div>
+
       <div className="new-song">
         <h1>新歌首发</h1>
         <div className="tab">
           {newSongTab.map((value, index) =>
             <span
-              key={value.id}
+              key={value.id || index}
               onClick={() => getPlaylist(setTabColorIndexNewSong, setNewSongCat, index, value.id)}
               style={{color: index === tabColorIndexNewSong ? "#31c27c" : "#000"}}
             >
             {value.name}
           </span>)
           }
+        </div>
+        <div className="slide-container" >
+          <div className="prev-button">
+            <span className="iconfont icon-shangyiye" onClick={() => chooseNewSong.current.prev()}/>
+          </div>
+          <div className="next-button">
+            <span className="iconfont icon-xiayiye" onClick={() => chooseNewSong.current.next()}/>
+          </div>
+          <Carousel ref={chooseNewSong}>
+            {newSongCarousel(0,9)}
+            {newSongCarousel(9,18)}
+            {newSongCarousel(18,27)}
+            {newSongCarousel(27,36)}
+          </Carousel>
+
         </div>
       </div>
     </div>
