db6e17c906b8c983a85673c9a8540246664baa73
BeADre <beadre_wang@163.com>
2019-09-30 00:29:59 +0800

    开发播放页面

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,25 +7,28 @@ import {Input} from 'antd';
 const {Search} = Input;
 
 const Index = () => {
+  const {pathname} = window.location;
+
   return (
     <BrowserRouter>
-      <div className="main-tab">
-        <ul>
-          {Router.map(value => (
-            <li key={value.title}>
-              <NavLink exact to={value.path} activeStyle={{color: "#31c27c"}}>{value.title}</NavLink>
-            </li>))}
-        </ul>
-        <div className="search">
-          <Search
-            placeholder="搜索"
-            onSearch={value => console.log(value)}
-          />
-        </div>
-
-      </div>
+      {pathname.includes("/playMusic") ? null :
+        <div className="main-tab">
+          <ul>
+            {Router.map(value => {
+              return value.hidden ? null : <li key={value.title}>
+                <NavLink exact to={value.path} activeStyle={{color: "#31c27c"}}>{value.title}</NavLink>
+              </li>
+            })}
+          </ul>
+          <div className="search">
+            <Search
+              placeholder="搜索"
+              onSearch={value => console.log(value)}
+            />
+          </div>
+        </div>}
       <Switch>
-        <Route exact path="/" render={()=> <Redirect to="/home" />}/>
+        <Route exact path="/" render={() => <Redirect to="/home"/>}/>
         {Router.map(value => <Route path={value.path} key={value.title} exact component={value.component}/>)}
       </Switch>
     </BrowserRouter>
