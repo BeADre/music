054b0ed1903523c0f715b4ae13df35a7a294f379
wanghao <wanghao@arhieason.com>
2019-10-30 18:22:20 +0800

    编写搜索页面

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React,{Fragment} from "react"
+import React,{Suspense} from "react"
 import { NavLink, Route, Switch, Redirect, withRouter} from "react-router-dom";
 import "./index.scss"
 import Router from "../router"
@@ -20,28 +20,30 @@ const hiddenRouterArr = Router.reduce((pre,cur) => {
 const Index = ({ location = {},history }) => {
   const {pathname} = location;
   return (
-    <Fragment>
-      {hiddenRouterArr.includes(pathname) ? null :
-        <div className="main-tab">
-          <ul>
-            {Router.map(value => {
-              return value.hidden ? null : <li key={value.title}>
-                <NavLink exact to={value.path} activeStyle={{color: "#31c27c"}}>{value.title}</NavLink>
-              </li>
-            })}
-          </ul>
-          <div className="search">
-            <Search
-              placeholder="搜索"
-              onSearch={value => searchHandel(value,history)}
-            />
-          </div>
-        </div>}
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/home" />}/>
-        {Router.map(value => <Route path={value.path} key={value.title} exact component={value.component}/>)}
-      </Switch>
-    </Fragment>
+
+      <Suspense fallback={<div>loading.....</div>}>
+        {hiddenRouterArr.includes(pathname) ? null :
+          <div className="main-tab">
+            <ul>
+              {Router.map(value => {
+                return value.hidden ? null : <li key={value.title}>
+                  <NavLink exact to={value.path} activeStyle={{color: "#31c27c"}}>{value.title}</NavLink>
+                </li>
+              })}
+            </ul>
+            <div className="search">
+              <Search
+                placeholder="搜索"
+                onSearch={value => searchHandel(value,history)}
+              />
+            </div>
+          </div>}
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/home" />}/>
+          {Router.map(value => <Route path={value.path} key={value.title} exact component={value.component}/>)}
+        </Switch>
+      </Suspense>
+
   )
 }
 export default withRouter(Index)
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,15 @@
-import Home from "../pages/home"
-import singer from "../pages/singer"
-import newDisc from "../pages/newDisc"
-import rank from "../pages/rank"
-import playlist from "../pages/playlist"
-import radio from "../pages/radio"
-import MV from "../pages/MV"
-import album from "../pages/album"
-import play from "../pages/play"
-import search from "../pages/search"
+import {lazy} from "react"
 
 // hidden为true时为编程式导航
 export default [
-  {path:"/home",component: Home, title: "首页"},
-  {path:"/singer",component: singer, title: "歌手"},
-  {path:"/newDisc",component: newDisc, title: "新碟"},
-  {path:"/rank",component: rank, title: "排行榜"},
-  {path:"/playlist",component: playlist, title: "分类歌单"},
-  {path:"/radio",component: radio, title: "电台"},
-  {path:"/MV",component: MV, title: "MV"},
-  {path:"/album",component: album, title: "数字专辑"},
-  {path:"/playMusic",component: play, title: "播放页", hidden:true},
-  {path:"/search",component: search, title: "搜索页", hidden:true},
+  {path: "/home", component: lazy(() => import("../pages/home")), title: "首页"},
+  {path: "/singer", component: lazy(() => import("../pages/singer")), title: "歌手"},
+  {path: "/newDisc", component: lazy(() => import("../pages/newDisc")), title: "新碟"},
+  {path: "/rank", component: lazy(() => import("../pages/rank")), title: "排行榜"},
+  {path: "/playlist", component: lazy(() => import("../pages/playlist")), title: "分类歌单"},
+  {path: "/radio", component: lazy(() => import("../pages/radio")), title: "电台"},
+  {path: "/MV", component: lazy(() => import("../pages/MV")), title: "MV"},
+  {path: "/album", component: lazy(() => import("../pages/album")), title: "数字专辑"},
+  {path: "/playMusic", component: lazy(() => import("../pages/play")), title: "播放页", hidden: true},
+  {path: "/search", component: lazy(() => import("../pages/search")), title: "搜索页", hidden: true},
 ]
