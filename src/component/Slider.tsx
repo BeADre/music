565ac0a8f5f86ccb710a85c20af33eea3e6045ff
BeADre <beadre_wang@163.com>
2020-01-10 18:30:50 +0800

    编写slider

--- src/component/Slider.tsx
+++ src/component/Slider.tsx
@@ -1,16 +1,37 @@
-import React, {RefObject, memo} from "react";
-import "./index.scss";
+import React, {MouseEvent,DragEvent, RefObject, useRef} from "react";
+import "./slider.scss";
 
 type Props = {
-
+  distance: number,
+  bufferTime: number
+  playTime: number
 }
 
 function Slider() {
-  return (
-    <>
+  const block = useRef<HTMLDivElement>(null);
+  const play = useRef<HTMLDivElement>(null);
+  const buffer = useRef<HTMLDivElement>(null);
+  const sliderClick = (e: MouseEvent) => {
+    (block.current as HTMLDivElement).style.left = `${e.clientX - 12}px`;
+    (play.current as HTMLDivElement).style.width = `${e.clientX - 12}px`;
 
-    </>
+  }
+  const sliderDown = (e: DragEvent) => {
+    if(e.clientX){
+      (block.current as HTMLDivElement).style.left = `${e.clientX - 12}px`;
+      (play.current as HTMLDivElement).style.width = `${e.clientX - 12}px`;
+    }
+  }
+  return (
+    <div className="slider-container">
+      <div className="slider-content" onClick={sliderClick}>
+        <div className="white-slider" />
+        <div className="play-slider" ref={play} />
+        <div className="buffer-slider" ref={buffer} />
+        <div className="slider-block" ref={block} draggable={true} onDrag={sliderDown}  />
+      </div>
+    </div>
   )
 }
 
-export default memo(Slider)
+export default Slider
