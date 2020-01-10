ce77a9234b1e9f43c12d9721f912d41dafaba57a
beadre_wang <beadre_wang@163.com>
2020-01-11 02:48:53 +0800

    slider编写完成

---
+++ src/component/PlaySlider.tsx
@@ -0,0 +1,67 @@
+import React, {MouseEvent, DragEvent, useRef, useEffect, useState} from "react";
+import "./slider.scss";
+
+type Props = {
+  bufferTimeValue: number
+  playWidth: string
+  onChange: Function
+}
+
+function PlaySlider({bufferTimeValue, playWidth, onChange}: Props) {
+  const [sliderState, setSliderState] = useState({
+    blockLeft: 0,
+    playWidth: 0,
+    bufferWidth: 0
+  });
+  const block = useRef<HTMLDivElement>(null);
+  const play = useRef<HTMLDivElement>(null);
+  const buffer = useRef<HTMLDivElement>(null);
+  const content = useRef<HTMLDivElement>(null);
+  const minWidth = content.current ? content.current.offsetWidth / 100 : 0
+  useEffect(() => {
+    setSliderState({
+      blockLeft: isNaN(+playWidth * minWidth) ? 0 : +playWidth * minWidth,
+      playWidth: isNaN(+playWidth * minWidth) ? 0 : +playWidth * minWidth,
+      bufferWidth: isNaN(bufferTimeValue * minWidth) ? 0 : bufferTimeValue * minWidth,
+    });
+  }, [playWidth, bufferTimeValue])
+
+  const changeHandle = (distance: number): void => {
+    const value = Math.ceil(distance / (+(content.current as HTMLDivElement).offsetWidth) * 100);
+    setSliderState({
+      ...sliderState,
+      ...{
+        blockLeft: distance,
+        playWidth: distance,
+      }
+    });
+    onChange(value)
+  }
+
+  const sliderClick = (e: MouseEvent): void => {
+    const distance = e.clientX - e.currentTarget.getBoundingClientRect().left - 6;
+    changeHandle(distance)
+  }
+
+  const sliderDown = (e: DragEvent): void => {
+    if (e.clientX) {
+      const distance = e.clientX - (content.current as HTMLDivElement).getBoundingClientRect().left - 6;
+      changeHandle(distance)
+    }
+  }
+  return (
+    <div className="slider-container">
+      <div className="slider-content" onClick={sliderClick} ref={content}>
+        <div className="white-slider"/>
+        <div className="play-slider" ref={play} style={{width: sliderState.playWidth}}/>
+        <div className="buffer-slider" ref={buffer} style={{width: sliderState.bufferWidth}}/>
+        <div className="slider-block"
+             style={{left: sliderState.blockLeft}}
+             ref={block}
+             draggable={true} onDrag={sliderDown}/>
+      </div>
+    </div>
+  )
+}
+
+export default PlaySlider
