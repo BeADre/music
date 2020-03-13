9e73bea3aa62c829e7a42b85ae97ba94e0d584a6
BeADre <beadre_wang@163.com>
2020-03-13 21:54:06 +0800

    修改路由展示问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,18 +5,18 @@ interface Router {
   path: string,
   component: FC,
   title: string,
-  hidden?: boolean
+  show?: boolean
 }
 
 const routerArr: Array<Router> = [
-  {path: "/home", component: lazy(() => import("../pages/home")), title: "首页"},
-  {path: "/singer", component: lazy(() => import("../pages/singer")), title: "歌手"},
-  // {path: "/newDisc", component: lazy(() => import("../pages/newDisc")), title: "新碟"},
-  // {path: "/rank", component: lazy(() => import("../pages/rank")), title: "排行榜"},
-  // {path: "/playlist", component: lazy(() => import("../pages/playlist")), title: "分类歌单"},
-  {path: "/mv", component: lazy(() => import("../pages/MV")), title: "MV", hidden: true},
-  {path: "/playMusic", component: lazy(() => import("../pages/play")), title: "播放页", hidden: true},
-  {path: "/search", component: lazy(() => import("../pages/search")), title: "搜索页", hidden: true},
-  {path: "*", component: lazy(() => import("../pages/notFind")), title: "404", hidden: true},
+  {path: "/home", component: lazy(() => import("../pages/home")), title: "首页", show: true},
+  {path: "/singer", component: lazy(() => import("../pages/singer")), title: "歌手", show: true},
+  // {path: "/newDisc", component: lazy(() => import("../pages/newDisc")), title: "新碟",show: true},
+  // {path: "/rank", component: lazy(() => import("../pages/rank")), title: "排行榜",show: true},
+  // {path: "/playlist", component: lazy(() => import("../pages/playlist")), title: "分类歌单",show: true},
+  {path: "/mv", component: lazy(() => import("../pages/MV")), title: "MV"},
+  {path: "/playMusic", component: lazy(() => import("../pages/play")), title: "播放页"},
+  {path: "/search", component: lazy(() => import("../pages/search")), title: "搜索页"},
+  {path: "*", component: lazy(() => import("../pages/notFind")), title: "404"},
 ];
 export default routerArr
