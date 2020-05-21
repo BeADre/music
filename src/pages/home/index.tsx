fe7ec640022b5d6d8b2b10bba566292276ccd1d6
wanghao <beadre_wang@163.com>
2020-05-21 18:38:26 +0800

    修改滚动模式

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,14 @@
-import React, {useState, useEffect, useRef, Fragment, useMemo, ReactNode, RefObject} from "react";
-import {Carousel, Icon} from "antd";
+import React, {useState, useEffect, Fragment, useMemo} from "react";
+import {Icon} from "antd";
 import {useSelector, useDispatch} from "react-redux";
 import utils from "../../utils";
 import {newSongTab, mvTab} from "../../staticData/home";
-import ChangeCarousel from "../../component/ChangeCarousel";
 import "./index.scss";
 
 function Home({history}: any) {
   const dispatch = useDispatch();
   const home = useSelector(({home}: any) => home);
   const {playTab = [], playlist = [], newSong = [], newPlate = [], mv = []} = home;
-  const choosePlaylist = useRef<Carousel>(null);
-  const chooseNewSong = useRef<Carousel>(null);
-  const chooseNewPlate = useRef<Carousel>(null);
-  const chooseMV = useRef<Carousel>(null);
   const [state, setState]: any = useState({
     tabColorIndexPlaylist: 0,
     tabColorIndexNewSong: 0,
@@ -72,8 +67,8 @@ function Home({history}: any) {
     })
   };
 
-  const tab = (tabName: Array<any>, stateProps: Array<string>) => {
-    return <div className="tab">
+  const tab = (tabName: Array<any>, stateProps: Array<string>, isFirst?: boolean) => {
+    return <div className="tab" id={isFirst ? "tab" : ""}>
       {tabName.map((value, index) =>
         <span
           key={value.id || index}
@@ -86,35 +81,6 @@ function Home({history}: any) {
     </div>
   };
 
-  // 第一部分遍历的歌单推荐内容
-  const memorizedPlay = useMemo(() => {
-    const copyPlaylist = [...playlist];
-    const slideElementArr = [];
-    let key = 1;
-    while (copyPlaylist.length) {
-      key++;
-      slideElementArr.push(
-        <div className="slide" key={key}>
-          {copyPlaylist.splice(0, 5).map((value, index) => (
-            <div key={index} className="slideContent">
-              <div className="slideContent-top" onClick={() => utils.jumpToPlay(history, value.id, false)}>
-                <div className="slide-keep">
-                  <span className="iconfont icon-ziyuan"/>
-                </div>
-                <img src={value.coverImgUrl} alt=""/>
-              </div>
-              <div className="slideContent-bot">
-                <span>{value.name}</span>
-                <p>播放量：{utils.unitCount(value.playCount)}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      )
-    }
-    return slideElementArr
-  }, [JSON.stringify(playlist)]);
-
   // 第二部分遍历的新歌内容
   const memorizedSong = useMemo(() => {
     const copyNewSong = [...newSong].splice(0, 45);
@@ -123,33 +89,35 @@ function Home({history}: any) {
     while (copyNewSong.length) {
       key++;
       slideElementArr.push(
-        <div className="slide" key={key}>
-          {copyNewSong.splice(0, 9).map((value) =>
-            <div className="slideContent2" key={value.id}>
-              <div className="slideContent-left" onClick={() => utils.jumpToPlay(history, value.id, true)}>
-                <div className="slide-keep">
-                  <span className="iconfont icon-ziyuan"/>
+        <div style={{display: "inline-block", whiteSpace: 'nowrap'}} key={key}>
+          <div className="slide">
+            {copyNewSong.splice(0, 9).map((value) =>
+              <div className="slideContent2" key={value.id}>
+                <div className="slideContent-left" onClick={() => utils.jumpToPlay(history, value.id, true)}>
+                  <div className="slide-keep">
+                    <span className="iconfont icon-ziyuan"/>
+                  </div>
+                  <img src={value.album.picUrl} alt=""/>
                 </div>
-                <img src={value.album.picUrl} alt=""/>
-              </div>
-              <div className="slideContent-right">
-                <div className="right-detail">
-                  <p>
-                    <span onClick={() => utils.jumpToPlay(history, value.id, true)}>{value.name}</span>
-                  </p>
-                  <p>
-                    {value.artists.map((artist: any, index: number) =>
-                      <Fragment key={artist.id}>
-                        <a>{artist.name}</a>
-                        {index === value.artists.length - 1 ? "" : <span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>}
-                      </Fragment>
-                    )}
-                  </p>
+                <div className="slideContent-right">
+                  <div className="right-detail">
+                    <p>
+                      <span onClick={() => utils.jumpToPlay(history, value.id, true)}>{value.name}</span>
+                    </p>
+                    <p>
+                      {value.artists.map((artist: any, index: number) =>
+                        <Fragment key={artist.id}>
+                          <a>{artist.name}</a>
+                          {index === value.artists.length - 1 ? "" : <span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>}
+                        </Fragment>
+                      )}
+                    </p>
+                  </div>
+                  <div className="song-time">{utils.unitTime(value.duration)}</div>
                 </div>
-                <div className="song-time">{utils.unitTime(value.duration)}</div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       )
     }
@@ -164,32 +132,34 @@ function Home({history}: any) {
     while (copyNewPlate.length) {
       key++;
       slideElementArr.push(
-        <div className="slide" key={key}>
-          {copyNewPlate.splice(0, 10).map(value => (
-            <div key={value.id} className="slideContent" style={{marginBottom: "20px"}}>
-              <div className="slideContent-top" onClick={() => utils.jumpToPlay(history, value.id, false, true)}>
-                <div className="slide-keep">
-                  <span className="iconfont icon-ziyuan"/>
+        <div style={{display: "inline-block", whiteSpace: 'nowrap'}} key={key}>
+          <div className="slide" key={key}>
+            {copyNewPlate.splice(0, 10).map(value => (
+              <div key={value.id} className="slideContent" style={{marginBottom: "20px"}}>
+                <div className="slideContent-top" onClick={() => utils.jumpToPlay(history, value.id, false, true)}>
+                  <div className="slide-keep">
+                    <span className="iconfont icon-ziyuan"/>
+                  </div>
+                  <img src={value.picUrl} alt=""/>
                 </div>
-                <img src={value.picUrl} alt=""/>
-              </div>
-              <div className="slideContent-bot">
-                <span
-                  onClick={() => utils.jumpToPlay(history, value.id, false, true)}
-                  className="section-three-name"
-                >
-                  {value.name}
-                </span>
-                <div className="section-three-art">
-                  {value.artists.map((artist: any, index: number) =>
-                    <Fragment key={artist.id}>
-                      {index === value.artists.length - 1 ? <p>{artist.name}</p> : <p>{artist.name}/</p>}
-                    </Fragment>
-                  )}
+                <div className="slideContent-bot">
+                  <span
+                    onClick={() => utils.jumpToPlay(history, value.id, false, true)}
+                    className="section-three-name"
+                  >
+                    {value.name}
+                  </span>
+                  <div className="section-three-art">
+                    {value.artists.map((artist: any, index: number) =>
+                      <Fragment key={artist.id}>
+                        {index === value.artists.length - 1 ? <p>{artist.name}</p> : <p>{artist.name}/</p>}
+                      </Fragment>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       )
     }
@@ -203,71 +173,87 @@ function Home({history}: any) {
     let key = 1;
     while (copyMv.length) {
       key++;
-      slideElementArr.push(<div className="slide" key={key}>
-        {copyMv.splice(0, 10).map(value =>
-          <div className="slideContent" key={value.id} style={{marginBottom: "20px"}}>
-            <div className="slideContent-top" onClick={() => utils.jumpToMv(history, value.id)}>
-              <div className="slide-keep">
-                <span className="iconfont icon-ziyuan"/>
+      slideElementArr.push(
+        <div style={{display: "inline-block", whiteSpace: 'nowrap'}} key={key}>
+          <div className="slide" key={key}>
+            {copyMv.splice(0, 10).map(value =>
+              <div className="slideContent" key={value.id} style={{marginBottom: "20px"}}>
+                <div className="slideContent-top" onClick={() => utils.jumpToMv(history, value.id)}>
+                  <div className="slide-keep">
+                    <span className="iconfont icon-ziyuan"/>
+                  </div>
+                  <img src={value.cover} alt=""/>
+                </div>
+                <div className="slideContent-bot">
+                  <span className="section-four-name" onClick={() => utils.jumpToMv(history, value.id)}>{value.name}</span>
+                  <p className="section-four-art">{value.artistName}</p>
+                  <p style={{marginTop: 20}}>
+                    <Icon type="video-camera"/>
+                    <span
+                      style={{
+                        marginLeft: "5px",
+                        display: "inline",
+                        cursor: "auto",
+                        color: "#999"
+                      }}
+                    >
+                      {utils.unitCount(value.playCount)}
+                    </span>
+                  </p>
+                </div>
               </div>
-              <img src={value.cover} alt=""/>
-            </div>
-            <div className="slideContent-bot">
-              <span className="section-four-name" onClick={() => utils.jumpToMv(history, value.id)}>{value.name}</span>
-              <p className="section-four-art">{value.artistName}</p>
-              <p style={{marginTop: 20}}>
-                <Icon type="video-camera"/>
-                <span
-                  style={{
-                    marginLeft: "5px",
-                    display: "inline",
-                    cursor: "auto",
-                    color: "#999"
-                  }}
-                >
-                  {utils.unitCount(value.playCount)}
-                </span>
-              </p>
-            </div>
+            )}
           </div>
-        )}
-      </div>)
+        </div>
+      )
     }
     return slideElementArr
   }, [JSON.stringify(mv)]);
-
-  const slideContainer = (refEle: RefObject<Carousel>, mapArr: Array<ReactNode | null>) => {
-    return <div className="slide-container">
-      <ChangeCarousel refEle={refEle}/>
-      <Carousel ref={refEle}>
-        {mapArr}
-      </Carousel>
-    </div>
-  };
 
   return (
     <div className="home-container">
       <div className="recommend-playlist">
         <h1>歌单推荐</h1>
-        {tab(playTab, ["tabColorIndexPlaylist", "playlistCat", "name"])}
-        {slideContainer(choosePlaylist, memorizedPlay)}
+        {tab(playTab, ["tabColorIndexPlaylist", "playlistCat", "name"], true)}
+        <div className="slide-container">
+          {playlist.map((value: any, index: number) => (
+            <div key={index} className="slideContent">
+              <div className="slideContent-top" onClick={() => utils.jumpToPlay(history, value.id, false)}>
+                <div className="slide-keep">
+                  <span className="iconfont icon-ziyuan"/>
+                </div>
+                <img src={value.coverImgUrl} alt=""/>
+              </div>
+              <div className="slideContent-bot">
+                <span>{value.name}</span>
+                <p>播放量：{utils.unitCount(value.playCount)}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="new-song">
         <h1>新歌首发</h1>
         {tab(newSongTab, ["tabColorIndexNewSong", "newSongCat", "id"])}
-        {slideContainer(chooseNewSong, memorizedSong)}
+        <div className="slide-container">
+          {memorizedSong}
+        </div>
       </div>
 
       <div className="new-plate">
         <h1>新碟首发</h1>
-        {slideContainer(chooseNewPlate, memorizedPlate)}
+        <div className="slide-container">
+          {memorizedPlate}
+        </div>
       </div>
 
       <div className="new-MV">
         <h1>MV</h1>
         {tab(mvTab, ["tabColorIndexMV", "mvCat", "name"])}
-        {slideContainer(chooseMV, memorizedMv)}
+        <div className="slide-container">
+          {memorizedMv}
+        </div>
       </div>
     </div>
   )
