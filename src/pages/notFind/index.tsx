0f43cb9f85fd2d315dc739778ab9a08efbc69949
beadre_wang <beadre_wang@163.com>
2020-06-14 23:31:22 +0800

    添加响应式

--- src/pages/notFind/index.tsx
+++ src/pages/notFind/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Result, Button, Icon } from 'antd';
+import "./index.less";
 
 const MyIcon = Icon.createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/font_1357705_wepgs1oohf.js',
@@ -10,11 +11,11 @@ function NotFind() {
     window.location.href = window.location.origin
   }
   return (
-    <div>
+    <div className={"not-found-container"}>
       <Result
         style={{color: "#1DA57A"}}
         status="info"
-        icon={<MyIcon type="icon-icon-test" style={{fontSize: 500}}/>}
+        icon={<MyIcon type="icon-icon-test" />}
         subTitle={<h1 style={{color:"#1DA57A", marginTop: -60}}>"哥们，你地址填错了吧-_-!"</h1>}
         extra={<Button type="primary" onClick={toHome}>回到首页</Button>}
       />
